ec6bd24525c993e951bec7323a90a3d1e7393a78
Linar <alr86@mail.ru>
2022-09-12 17:38:55 +0300

    filled task 1

--- Task_1.py
+++ Task_1.py
@@ -1,5 +1,29 @@
-# 1 - Напишите программу, которая принимает на вход вещественное число и показывает 
+# 1 - Напишите программу, которая принимает на вход вещественное число и показывает
 # сумму его цифр. Учтите, что числа могут быть отрицательными
 # Пример:
 # 67.82 -> 23
 # 0.56 -> 11
+
+def real_num():
+    try:
+        num = float(input("Enter real number: "))
+        return num
+    except ValueError:
+        print('Error')
+        return real_num()
+
+
+def find_sum():
+    num = real_num()
+    if num < 0:
+        num = abs(float(num))
+    sum = 0
+    num_str = str(num)
+    num_list = list(num_str)
+    for i in num_list:
+        if i != ".":
+            sum += int(i)
+    print(sum)
+
+
+find_sum()
